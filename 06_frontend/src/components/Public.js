a88e769e947ed6de06d8486e992819a756106890
Onroe <leninkaha@gmail.com>
2023-01-31 10:01:53 +0300

    update:frontend

--- 06_frontend/src/components/Public.js
+++ 06_frontend/src/components/Public.js
@@ -4,18 +4,18 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Just Hardware Repairshop !</span></h1>
+                <h1>Welcome to <span className="nowrap">JF Repair Shop!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Kampala City, the repair shop  provides a trained staff ready to meet repair needs for our flagship products.</p>
+                <p>Located in Nakawa City, JF Repair Shop  provides a trained staff ready to meet your tech repair needs.</p>
                 <address className="public__addr">
-                Just Hardware Repairshop <br />
-                    6th Street Industrial Area<br />
-                    Nakawa<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                    
+                    555 Special Drive<br />
+                    
+                    <a href="tel:+15555555555">(222) 222-2222</a>
                 </address>
                 <br />
-                <p>Owner: Ogundipe</p>
+                <p>Owner: Banda Balogo</p>
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
